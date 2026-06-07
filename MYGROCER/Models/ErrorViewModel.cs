77e072d25404f215584ffab417d64c168fede813
qai <138949452+qaiou@users.noreply.github.com>
2026-06-07 16:05:37 +0800

    Add project files.

---
+++ MYGROCER/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYGROCER.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
