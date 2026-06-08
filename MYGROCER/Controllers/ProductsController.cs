df0930e52ae5280d7266e736deec1020a084b1a3
qai <138949452+qaiou@users.noreply.github.com>
2026-06-08 23:18:26 +0800

    Added products mvc

---
+++ MYGROCER/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MYGROCER.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MYGROCER/Models/ProductsModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MYGROCER.Models
+{
+    public class ProductsModel
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string? Description { get; set; }
+
+        [MaxLength(50)]
+        public string? Category { get; set; }
+
+        [Column(TypeName = "decimal(20,2)")]
+        public decimal BasePrice { get; set; }
+
+
+    }
+}
