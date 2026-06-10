e64d1c4d8d7e4bdbe6e85adaf5a1451954eb0aac
qai <138949452+qaiou@users.noreply.github.com>
2026-06-10 23:48:20 +0800

    shopping cart interface

---
+++ Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MYGROCER.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
