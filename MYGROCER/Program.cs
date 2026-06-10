f909b577c10a1e7b088f20c668cc35c2c023636f
hinuraaa <nura05.lia@gmail.com>
2026-06-10 20:23:12 +0800

    update changes

--- MYGROCER/Program.cs
+++ MYGROCER/Program.cs
@@ -1,29 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using MYGROCER.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Register database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=mygrocer.db"));
+
+// Register Singleton
+var dbSingleton = DbConnectionSingleton.GetInstance("Data Source=mygrocer.db");
+builder.Services.AddSingleton(dbSingleton);
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Auto-create database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
